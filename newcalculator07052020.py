28efd072df8726538a05716b7e3d6d3cbf100d1d
DimitryLenchevsky <overborsch@gmail.com>
2020-05-07 17:35:15 +0300

    Все пункты готовы, но есть проблема - Данные перезаписыаются в словарь, а не создаются дополнительные

--- newcalculator07052020.py
+++ newcalculator07052020.py
@@ -1,51 +1,55 @@
+while True:
 
-first_name = input("Введите ваше имя: ")
-last_name = input("Введите Вашу фамилию: ")
-middle_name = input("Введите ваше отчество: ")
-weight = int(input("Введите массу тела в килограммах: "))
-height = int(input("Введите Ваш рост в сантиметрах: "))
-sex = input("Введите Ваш пол: ")
-age = int(input("Введите Ваш возраст: "))
+    first_name = input("Здравствтуйте. Что бы остановить программу, введите q. Для того что бы продолжить ведите ваше имя: ")
+    if first_name == "q":
+        break
+    else:
+        last_name = input("Введите Вашу фамилию: ")
+        middle_name = input("Введите ваше отчество: ")
+        weight = int(input("Введите массу тела в килограммах: "))
+        height = int(input("Введите Ваш рост в сантиметрах: "))
+        sex = input("Введите Ваш пол: ")
+        age = int(input("Введите Ваш возраст: "))
 
-bmi = weight / (height * height / 10000)
+        bmi = weight / (height * height / 10000)
 
-print("Индекс Вашей массы тела равен: ", bmi)    
-if bmi <= 16:                                    
-    print("Выраженный дефицит массы тела")
-elif bmi <= 18.5:
-    print("Недостаточная (дефицит) масса тела")
-elif bmi <= 25:
-    print("Ваш индекс массы тела в норме")
-elif bmi <= 30:
-    print("Избыточная масса тела (предожирение)")
-elif bmi <= 35:
-    print("Ожирение")
-elif bmi <= 40:
-    print("Ожирение резкое")
-elif bmi > 40:
-    print("Очень резкое ожирение")
-pass
+        print("Индекс Вашей массы тела равен: ", bmi)
+        if bmi <= 16:                                    
+            print("Выраженный дефицит массы тела")
+        elif bmi <= 18.5:
+            print("Недостаточная (дефицит) масса тела")
+        elif bmi <= 25:
+            print("Ваш индекс массы тела в норме")
+        elif bmi <= 30:
+            print("Избыточная масса тела (предожирение)")
+        elif bmi <= 35:
+            print("Ожирение")
+        elif bmi <= 40:
+            print("Ожирение резкое")
+        elif bmi > 40:
+            print("Очень резкое ожирение")
+        pass
 
-persons = {}
-info = [last_name, first_name, middle_name, weight, height, age, bmi, sex]
+        persons = {}
+        info = [last_name, first_name, middle_name, weight, height, age, bmi, sex]
 
-persons['Фамилия'] = info[0]
-persons['Имя'] = info[1]
-persons['Отчество'] = info[2]
-persons['Масса в кг'] = info[3]
-persons['Рост'] = info[4]
-persons['Возраст'] = info[5]
-persons['bmi'] = info[6]
-persons['Пол'] = info[7]
+        persons['Фамилия'] = info[0]
+        persons['Имя'] = info[1]
+        persons['Отчество'] = info[2]
+        persons['Масса в кг'] = info[3]
+        persons['Рост'] = info[4]
+        persons['Возраст'] = info[5]
+        persons['bmi'] = info[6]
+        persons['Пол'] = info[7]
 
-print(persons)
+        print(persons)
 
-# Вывод шкалы
-tick = "|"
-first = bmi - 10
-second = 50 - bmi
+        # Вывод шкалы
+        tick = "|"
+        first = bmi - 10
+        second = 50 - bmi
 
-scale = '10' + "=" * int(first) + tick + "=" * int(second) + "50"
-print(scale)
+        scale = '10' + "=" * int(first) + tick + "=" * int(second) + "50"
+        print(scale)
 
 
