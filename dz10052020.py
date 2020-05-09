f7a4d9685bde5c92eceeaf733f0343f06cde99a0
DimitryLenchevsky <overborsch@gmail.com>
2020-05-09 21:56:44 +0300

    сидел сидел и без теории что-то не смог ничего придумать

---
+++ dz10052020.py
@@ -0,0 +1,25 @@
+#def askname():
+    #name = input("Enter your name: ")
+    #f(name)
+
+
+#def f(name):
+    #print('Hello World! {}'.format(name))
+
+#askname()
+
+def start_menu():
+    print("\t1. Выход \n\
+    2. Добавить контакт \n\
+    3. Заменить контакт \n")
+    choose(choice)
+
+def choose(choice):
+    choice = int(input("\tВаш выбор: \n"))
+
+def exit():
+    if choice == 1:
+        exit()
+# пока полностью не разобрался без теории
+
+start_menu()
