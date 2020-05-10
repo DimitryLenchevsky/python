1c071afeb106269c08a35d6ca362dc61721b6b3b
DimitryLenchevsky <overborsch@gmail.com>
2020-05-10 16:43:19 +0300

    Эврика! Осталась только шкала

--- dz10052020.py
+++ dz10052020.py
@@ -17,6 +17,8 @@ def choose(choice):
         age = int(input("Введите Ваш возраст: "))
         value = [weight, height, sex, age]
         person[key] = value
+        bmi = weight / (height * height / 10000)
+        recomendation(bmi)
         conclusion_person()
     else:
             print("Упс, что-то пошло не так")
@@ -25,6 +27,29 @@ def conclusion_person():
     for key, value in person.items():
         print(key, ",", value)
 
+def recomendation(bmi):
+    if bmi <= 16:                                    
+        print("Выраженный дефицит массы тела, больше кушайте!")
+    elif bmi <= 18.5:
+        print("Недостаточная (дефицит) масса тела. Больше кушайте!")
+    elif bmi <= 25:
+        print("Ваш индекс массы тела в норме")
+    elif bmi <= 30:
+        print("Избыточная масса тела (предожирение), постарайтесь больше двигаться!")
+    elif bmi <= 35:
+        print("Ожирение, постарайтесь больше двигаться и меньше есть!")
+    elif bmi <= 40:
+        print("Ожирение резкое, срочно сходите к врачу!")
+    elif bmi > 40:
+        print("Очень резкое ожирение, срочно сходите к врачу!")
+
+def main():
+    while True:
+        start_menu()
+
+
+
+
 
 person = {}
-start_menu()
+main()
