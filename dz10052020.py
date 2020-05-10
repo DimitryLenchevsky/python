22978d36e814a83c2c9338879898dc66324e23e6
DimitryLenchevsky <overborsch@gmail.com>
2020-05-10 14:44:47 +0300

    пока четверть программы удалось сделать рабочей

--- dz10052020.py
+++ dz10052020.py
@@ -1,25 +1,28 @@
-#def askname():
-    #name = input("Enter your name: ")
-    #f(name)
-
-
-#def f(name):
-    #print('Hello World! {}'.format(name))
-
-#askname()
 
 def start_menu():
     print("\t1. Выход \n\
     2. Добавить контакт \n\
     3. Заменить контакт \n")
+    choice = int(input("\tВаш выбор: \n"))
     choose(choice)
 
 def choose(choice):
-    choice = int(input("\tВаш выбор: \n"))
-
-def exit():
     if choice == 1:
         exit()
-# пока полностью не разобрался без теории
+    elif choice == 2 or 3:
+        key = input("Введите ФИО: ")
+        weight = int(input("Введите массу тела в килограммах: "))
+        height = int(input("Введите Ваш рост в сантиметрах: "))
+        sex = input("Введите Ваш пол: ")
+        age = int(input("Введите Ваш возраст: "))
+    else:
+            print("Упс, что-то пошло не так")
+    #create_user()
 
+#def create_user:
+#далее когда пытаюсь создать пользователя в словаре, ловлю кучу ошибок.
+#На данном этапе код работает только в меню и сборе данных.
+
+
+person = {}
 start_menu()
